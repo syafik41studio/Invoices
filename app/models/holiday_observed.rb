efda0f295444804a15dcf9ba6b334010ff69e1f9
syafik41studio <syafik@41studio.com>
2011-12-27 07:43:58 +0700

    test

--- app/models/holiday_observed.rb
+++ app/models/holiday_observed.rb
@@ -3,6 +3,6 @@ class HolidayObserved < ActiveRecord::Base
   belongs_to :profile
 
   attr_accessor :simple_date
-  attr_accessible :simple_date
+  attr_accessible :simple_date, :holiday_date, :holiday_name, :holiday_description
   
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_many :holiday_observeds
 
-  accepts_nested_attributes_for :holiday_observeds
+  accepts_nested_attributes_for :holiday_observeds, :reject_if => :all_blank, :allow_destroy => true
   
   attr_accessor :full_name
   attr_accessible :full_name, :user_id, :speciality, :gender, :languages, :accepting_new_patient,
@@ -13,7 +13,7 @@ class Profile < ActiveRecord::Base
     :accepting_new_patient_pv, :about_me_pv, :biography_pv, :practice_pv, :training_and_cert_pv,
     :work_phone_pv, :ext_pv, :work_cell_phone_pv, :work_fax_number_pv, :email_address_pv,
     :office_address_pv, :office_addresss_2_pv, :city_pv,:state_pv, :zip_pv, :working_hours_pv,
-    :prefered_contact_method_pv, :holiday_observed_pv, :description, :description_pv
+    :prefered_contact_method_pv, :holiday_observed_pv, :description, :description_pv, :holiday_observeds_attributes
 
   after_save :set_user_full_name
 
